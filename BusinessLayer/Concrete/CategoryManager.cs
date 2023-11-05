2723ab664df902141cb2b676e2b2e8da939f7c30
muratapk <muratapk@hotmail.com>
2023-11-05 15:46:24 +0300

    donanım 22

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        public void TAdd(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> TGetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category TGetId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/ProductManager.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        public void TAdd(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> TGetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product TGetId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
